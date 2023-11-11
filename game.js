b864ffe22933f18ff5208881a4d14cc93c96be9f
Letsygo <150294350+Letsygo@users.noreply.github.com>
2023-11-11 09:41:56 -0700

    Update game.js

--- game.js
+++ game.js
@@ -3,31 +3,39 @@ const ctx = canvas.getContext('2d');
 resize();
 let x = canvas.width/2;
 let y = canvas.height/2;
-tick();
+let sx = 0;
+let sy = 0;
 //key detecter
 var pressedKeys = {};
 window.onkeyup = function(e) { pressedKeys[e.keyCode] = false; }
 window.onkeydown = function(e) { pressedKeys[e.keyCode] = true; }
+tick();
 
 //tick
 function tick() {
   ctx.clearRect(0,0,canvas.width,canvas.height);
-  ctx.fillRect(x-5,y-5,10,10);
+  ctx.fillRect(x-5,canvas.height-(y+5),10,10);
+
+  //friction
+  sx *= 0.8;
+  sy *= 0.8;
 
   //controls
-  e = window.event;
-  if (pressedKeys[38]) {
-    y += 5;
+  if (key(38)) {
+    sy += 1.5;
   }
-  if (pressedKeys[40]) {
-    y -= 5;
+  if (key(40)) {
+   sy -= 1.5;
   }
-  if (pressedKeys[39]) {
-    x += 5;
+  if (key(39)) {
+    sx += 1.5;
   }
-  if (pressedKeys[37]) {
-    x -= 5;
+  if (key(37)) {
+    sx -= 1.5;
   }
+
+  x += sx;
+  y += sy;
 
   //other
   window.requestAnimationFrame(tick);
@@ -40,4 +48,8 @@ window.onresize = function(){
 function resize() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+}
+
+function key(code) {
+  return pressedKeys[code];
 }
