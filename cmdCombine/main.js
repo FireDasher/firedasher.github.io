6d34deb12a8c242c17a32f7231da6be37e7a09b7
FireDasher <150294350+FireDasher@users.noreply.github.com>
2024-08-23 17:00:18 -0700

    Create Command Combiner

---
+++ cmdCombine/main.js
@@ -0,0 +1,20 @@
+function $(q){return document.querySelector(q);}
+const input = $("#input");
+const output = $("#output");
+const delorig = $("#delorig");
+function fb(name) {
+    return {id:"falling_block",BlockState:{Name:name},Time:1};
+}
+function upd() {
+    const cmds = [...input.value.split(/\n/g), "gamerule commandBlockOutput false", `setblock ~ ~1 ~ command_block{auto:1,Command:\"fill ~ ~ ~ ~ ~${delorig.checked ? "-3" : "-2"} ~ air\"}`, "kill @e[type=command_block_minecart,distance=..1]"];
+    let obj = {BlockState:{Name:"redstone_block"},Time:1};
+    obj.Passengers = [fb("activator_rail")];
+    obj.Passengers[0].Passengers = [];
+    const cmdpass = obj.Passengers[0].Passengers;
+    cmds.forEach(e=>{
+        cmdpass.push({id: "command_block_minecart", Command: e});
+    });
+    output.value = "/summon falling_block ~ ~1 ~ " + JSON.stringify(obj);
+}
+input.oninput = upd;
+delorig.oninput = upd;
